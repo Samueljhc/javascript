1c5a2c861cbdc6cf12cd1522a293dac2b5e8f821
Ceti <155686120+Samueljhc@users.noreply.github.com>
2024-04-20 10:47:52 -0300

    JS e meio estranho

---
+++ exercicios-js/fundamentos.js/comentario.js
@@ -0,0 +1,11 @@
+// Comentarios de uma linha
+console.log("Linha 1");
+
+/*
+    Comentarios de
+    múltiplas linhas
+*/
+console.log("Linha 2");
+
+/* Comentário de múltiplas linhas */
+console.log("Linha 3");
