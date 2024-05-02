05669940d35f1e2cedd6d2eee8e6c9479147af76
Ceti <155686120+Samueljhc@users.noreply.github.com>
2024-05-02 11:05:08 -0300

    Aulas da Manhã

--- exercicios-js/fundamentos.js/UandoLetEmLoop1.js
+++ exercicios-js/fundamentos.js/UandoLetEmLoop1.js
@@ -1,4 +1,6 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i)
+for (let i = 0; i < 10; i++) { // scopo de bloco
+    console.log(i) // utiliza scopo de bloco {}
 }
-console.log('i =', i)
+
+// let i = 0 // scopo global
+console.log('i =', i) // utiliza scopo global
--- exercicios-js/fundamentos.js/UsandoVarEmLoop1.js
+++ exercicios-js/fundamentos.js/UsandoVarEmLoop1.js
@@ -1,4 +1,4 @@
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 10; i++) { /* var não tem scopo de bloco */
     console.log(i)
 }
 console.log('i =', i)
