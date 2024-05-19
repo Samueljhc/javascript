8b8b6cb2083418c8936995d2073453d4cab51f73
Samuel Ayala <Samuelayala303@gmail.com>
2024-05-19 15:08:03 -0300

    sorvete

---
+++ exercicios-js/fundamentos.js/aritmeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c ,d] = [3, 5, 1, 15]
+
+const adicao = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(adicao, subtracao, multiplicacao, divisao, modulo)
+
+
+// ++d / prefixo / antes do operando
+// op1 + op2 / infix / operador binário
