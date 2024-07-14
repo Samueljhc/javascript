eaa5c56b976647012ac4bb906ea6161b48b85109
Samuel Ayala <155686120+Samueljhc@users.noreply.github.com>
2024-07-14 10:56:10 -0300

    Objetos
    Inicio da seção de objetos.

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+// Código não executável!!!
+
+// Procedural
+procedimento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    procedimento() {
+        // ...
+    }
+}
+
+objeto.procedimento() // foco passou a ser o objeto
+
+/* Metodos da OO
+1. Abstração
+2. Encapsulamento
+3. Herança (prototype)
+4. Polimorfismo
+*/
